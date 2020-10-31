547a2d9eafd39d7c14018c764e9f59b591ac1899
iamdiksha <bhatiadikshita@gmail.com>
2020-11-01 03:26:48 +0530

    Displaying Posts(presently not working)

--- includes/header.php
+++ includes/header.php
@@ -1,23 +1,69 @@
-<?php
+<?php  
 require 'config/config.php';
-if(isset($_SESSION['username'])){
-    $userloggedIn=$_SESSION['username'];
+
+
+if (isset($_SESSION['username'])) {
+	$userLoggedIn = $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_details_query);
 }
-else{
-    header("Location:register_new.php");
+else {
+	header("Location: register.php");
 }
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+	<title>Welcome to Socionet</title>
+
+	<!-- Javascript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/stylea.css">
+	<link href="https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Grandstander:wght@600&display=swap" rel="stylesheet">
 </head>
 <body>
-    HI
-</body>
-</html>
+
+	<div class="top_bar"> 
+
+		<div class="logo">
+			<a href="index.php">Socionet!</a>
+		</div>
+
+		<nav>
+			<a href="<?php echo $userLoggedIn; ?>">
+				<?php echo $user['first_name']; ?>
+			</a>
+			<a href="index.php">
+				<i class="fa fa-home fa-lg"></i>
+			</a>
+			<a href="#">
+			<i class="fa fa-comments-o" ></i>
+			</a>
+			<a href="#">
+			<i class="fa fa-bell-o" ></i>
+			</a>
+			<a href="#">
+				<i class="fa fa-users fa-lg"></i>
+			</a>
+			<a href="#">
+			<i class="fa fa-cogs" ></i>
+			</a>
+			<a href="includes/handlers/logout.php">
+				<i class="fa fa-sign-out fa-lg"></i>
+			</a>
+
+
+
+		</nav>
+
+	</div>
+
+
+	<div class="wrapper">
--- index.php
+++ index.php
@@ -1,13 +1,52 @@
-<?php  
-require 'config/config.php';
+<?php 
+include("includes/header.php");
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
 
-?>
 
-<html>
-<head>
-	<title>Welcome to Socionet</title>
-</head>
-<body>
-	Hello 
+if(isset($_POST['post'])){
+	$post = new Post($con, $userLoggedIn);
+	$post->submitPost($_POST['post_text'], 'none');
+}
+
+
+ ?>
+	<div class="user_details column">
+		<a href="<?php echo $userLoggedIn; ?>">  <img src=assets/images/profile_pics/female.jpg> </a>
+
+		<div class="user_details_left_right">
+			<a href="<?php echo $userLoggedIn; ?>">
+			<?php 
+			echo $user['first_name'] . " " . $user['last_name'];
+            ?>
+			</a>
+			<br>
+			
+		</div>
+
+	</div>
+
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<hr>
+
+		</form>
+
+		<?php 
+
+		$user_obj = new User($con, $userLoggedIn);
+		echo $user_obj->getFirstAndLastName();
+
+		?>
+
+
+	</div>
+
+
+
+
+	</div>
 </body>
 </html>
